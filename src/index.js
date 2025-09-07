4102b54cb627645677058a12b29707cb4b8c01be
vupwoz <vupwoz@gmail.com>
2025-09-07 17:02:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import "./styles.css"; // 👈 This line ensures Tailwind is applied
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
